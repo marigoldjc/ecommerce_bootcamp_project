03e67c86fbe875c0a90970f4c62e45518f34301a
marigoldjc <142034334+marigoldjc@users.noreply.github.com>
2024-11-10 16:59:19 +0800

    Update models.py

--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -16,5 +16,16 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-class Cart(models.Model):
-    items = models.JSONField(default=dict)  # Store cart items as a dictionary
+class Order(models.Model):
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
